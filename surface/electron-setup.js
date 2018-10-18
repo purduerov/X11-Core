acf956222263128990c6812be3dc26c0f4fa3961
Tobi Ola <oluwatobi.ola@gmail.com>
2018-10-17 20:07:53 -0400

    fixed a stupid error

--- surface/electron-setup.js
+++ surface/electron-setup.js
@@ -1,8 +1,8 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
-const spawn = require('child_process');
-spawn = spawn.spawn;
+const childProcess = require('child_process');
+const spawn = childProcess.spawn;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
