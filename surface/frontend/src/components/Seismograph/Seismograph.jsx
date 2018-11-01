00e7788d7b6779ba961448b82750f7c65ac0d83b
isibley <isibley@purdue.edu>
2018-11-01 00:12:49 -0400

    Added old components, pushed them through linting

---
+++ surface/frontend/src/components/Seismograph/Seismograph.jsx
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import styles from './Seismograph.css';
+import SeismographGraph from './SeismographGraph.jsx';
+
+// import './GoogleChartLibrary.js';
+
+// google.charts.load('current', {'packages':['corechart']});
+// google.charts.setOnLoadCallback(drawChart);
+
+export default class Seismograph extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { graph: false, points: [] };
+
+        // this.state = {time: props.time, amp: props.amplitude};
+        // console.log(props.time+", "+props.amplitude)
+
+        this.disableRend = this.disableRend.bind(this);
+        this.rendTime = this.rendTime.bind(this);
+        this.rendAmp = this.rendAmp.bind(this);
+        this.load = this.load.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
+        this.chartStuff = this.chartStuff.bind(this);
+    }
+
+    rendTime() {
+        return this.props.time.map((val, index) => (
+            <div key={`time${index}`}>
+                <hr className={styles.squashed} />
+                <div className={styles.time}>{val}</div>
+            </div>
+        ));
+    }
+
+    chartStuff() {
+        return (
+            <div id="chart_div" style="width: 900px; height: 500px;" />
+
+        );
+    }
+
+    rendAmp() {
+        return this.props.amplitude.map((val, index) => (
+            <div key={`amp${index}`}>
+                <hr className={styles.squashed} />
+                <div className={styles.amplitude}>{val}</div>
+            </div>
+        ));
+    }
+
+    load() {
+        var string = ($('#data').val());
+        var pnts = [];
+        string.split(', ').forEach((val, i) => {
+            console.log(`${i}: ${parseFloat(val)}`);
+            pnts.push(parseFloat(val));
+        });
+        this.setState({
+            points: pnts,
+        }, () => {
+        // console.log(this.state);
+        });
+    }
+
+    disableRend() {
+        this.setState({
+            graph: false,
+        });
+    }
+
+    componentDidMount() {
+        $(`.${styles.spawnButton}`).click(() => {
+            this.setState({
+                graph: true,
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div className={styles.container}>
+                <div className={styles.halfRight}>
+                    <div>Times:</div>
+                    {this.rendTime()}
+                </div>
+                <div className={styles.halfLeft}>
+                    <div>Amplitudes:</div>
+                    {this.rendAmp()}
+                </div>
+                <div className={styles.bottom}>
+                    <input id="data" defaultValue="Insert comma Ordered List" />
+                    <button id="load" onClick={this.load}>Load Data</button>
+                </div>
+                {
+                    this.state.graph
+                        ? (
+                            <SeismographGraph
+                              amplitude={this.state.points}
+                              rend={this.disableRend}
+                            />
+                        ) : <p>"Press the button to generate a graph"</p>
+                }
+                <button className={styles.spawnButton}>Spawn Graph</button>
+            </div>
+        );
+    }
+}
---
+++ surface/frontend/src/components/ToolView/ToolView.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import SliderControl from '../SliderControl/SliderControl.jsx';
+import styles from './ToolView.css';
+
+export default class ToolView extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { conf: props.conf };
+        this.axis = ['manipulator', 'obs_tool', 'servo', 'transmitter', 'magnet'];
+
+        this.rendTool = this.rendTool.bind(this);
+        this.reRender = this.reRender.bind(this);
+    }
+
+    reRender(val, inv, tool) {
+        const confcpy = this.state.conf;
+        // console.log(e.target.id)
+        confcpy[tool].invert = inv;
+        confcpy[tool].master = val / 100;
+        this.props.rend(confcpy);
+        /* this.setState({conf: confcpy}, function() {
+          this.props.rend(this.state.val, this.state.inv, this.props.indx);
+          //console.log(this.state.inv);
+      }); */
+    }
+
+    rendTool() {
+        return this.axis.map((val, index) => (
+            <li key={`axis${val}`}>
+                {this.state.conf[val] == undefined && (
+                    <p className={styles.lowMargin}>
+                        {val}
+:
+                        {' '}
+                        {typeof this.props[val] === 'boolean' ? (this.props[val] ? 1 : 0) : this.props[val].toFixed(2)}
+                    </p>
+                )}
+                {this.state.conf[val] != undefined
+          && <SliderControl min="0" max="50" key={val} indx={val} power={this.state.conf[val].master * 100} invert={this.state.conf[val].invert} rend={this.reRender} name={val} />}
+            </li>
+        ));
+    }
+
+    render() {
+        return (
+            <div className={styles.container}>
+                <div className={styles.ToolView}>
+                    <ul>
+                        {this.rendTool()}
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+} // end export
