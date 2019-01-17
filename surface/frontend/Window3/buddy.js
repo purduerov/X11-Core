57eb4074c940b389c3133371875e9708fb7ec7f9
anawesumoposum <esibley@purdue.edu>
2019-01-17 18:54:10 -0500

    buddy class done

--- surface/frontend/Window3/buddy.js
+++ surface/frontend/Window3/buddy.js
@@ -1,49 +1,5 @@
 const { shell, app, ipcRenderer } = window.require('electron');
-
-class keyBind {
-  constructor(where) {
-      this.directionscpy = {x: 0, y: 0};
-
-      this.goodkeys = {
-        37: this.blankFunc('y', -1),
-        38: this.blankFunc('x', 1),
-        39: this.blankFunc('y', 1),
-        40: this.blankFunc('x', -1)
-      };
-      console.log(this.goodkeys);
-
-      $("body").keydown((event) => {
-        var key = event.which;
-        if (this.goodkeys[key] != undefined) {
-          this.goodkeys[key](1);
-        }
-      });
-
-      $("body").keyup((event) => {
-        var key = event.which;
-        if (this.goodkeys[key] != undefined) {
-          this.goodkeys[key](0);
-        }
-      });
-
-      setInterval(() => {
-        where.setState({
-          directions: this.directionscpy
-        });
-      }, 50);
-
-  }
-
-  blankFunc(key, posneg) {
-    // called like "blankFunc('x', -1)" or "blankFunc('y', 1)"
-    return (value) => {
-      if(!(this.directionscpy[key]) || this.directionscpy[key] == posneg) {
-        this.directionscpy[key] = value * posneg;
-        console.log(this.directionscpy[key]);
-      }
-    }
-  }
-}
+var keyBind = require('./keyBind.js');
 
 //object for buddy's control status
 //var rocketcpy = { 37: 0, 38: 0, 39: 0, 40: 0 };
@@ -78,5 +34,5 @@ module.exports = (where, socketHost) => {
         console.log(pocketrocket[key]);
       }
     }); */
-    const buddy = new keyBind(where);
+    var buddy = new keyBind(where);
 }; // end export
