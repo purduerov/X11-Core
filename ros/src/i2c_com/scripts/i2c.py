bfe0c414627a234394c26503d25877ccc277f06b
JeroSik <yoon161@purdue.edu>
2019-02-02 14:39:32 -0500

    Update with SMBus2

--- ros/src/i2c_com/scripts/i2c.py
+++ ros/src/i2c_com/scripts/i2c.py
@@ -1,17 +1,26 @@
 #! /usr/bin/python
 import rospy
 from shared_msgs.msg import i2c_msg
+from smbus2 import SMBusWrapper
 from threading import Thread, Lock
 
 #mutex = Lock()
 
 def message_received(msg):
   # This runs on a seperate thread from the pub
-  m = i2c_msg()
-  m.data = msg.data
-  m.addr = msg.addr
-  
-  rospy.loginfo(m)
+
+  # Read block of 8 bytes from 'i2c_tx', offest 0
+  with SMBusWrapper(1) as bus:
+    b = bus.read_i2c_block_data(i2c_tx, 0, 8)
+    b = bus.read_i2c_block_data(i2c_tx, 0, 64)
+    
+    # Set i2c_msg type
+    m = i2c_msg()
+    m.data = msg.data
+    m.addr = msg.addr
+
+    rospy.loginfo(m)
+
   pass
 
 if __name__ == "__main__":
@@ -22,10 +31,15 @@ if __name__ == "__main__":
       message_received)
 
   # TODO: I2C related activities
+  
+  # Set rate and tell ROS node name
   rospy.init_node('talker', anomymous=True)
   rate = rospy.Rate(10) # 10hz
 
+  # Get i2c_msg type
   msg = i2c_msg()
+  
+  # Publish msg to topic
   while not rospy.is_shutdown():
     rospy.loginfo(msg)
     pub.publish(msg)
