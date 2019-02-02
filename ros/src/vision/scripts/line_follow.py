773412a3b3bae045d4570e42aee2e3c908eb569d
Zach Ghera <zpghera00@gmail.com>
2019-02-02 14:02:21 -0500

    Added contouring and grabbing largest contour

--- ros/src/vision/scripts/line_follow.py
+++ ros/src/vision/scripts/line_follow.py
@@ -10,8 +10,18 @@ bridge = CvBridge()
 
 def get_largest(img):
   #grab all of the contours
+  im, contours, hierarchy = cv2.findContours(img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
   #iterate through contours to find one with largest area 
+  largest_area  = -1 
+  largest_cnt = -1
+  for i in range(len(contours)):
+    area = cv2.contourArea(contours[i])
+    if largest_area < area:
+	largest_area = area
+        largest_cnt = contours[i]
+
+  return largest_cnt
 
 #functions that manipulates the data that comes the camera
 def process(data):
@@ -25,13 +35,12 @@ def process(data):
   img = cv2.medianBlur(img,5)
   img = cv2.GaussianBlur(img,(5,5),0)
  
-  #color filtold for red at top and bottom of hue spectrum
   #red
   #img = cv2.inRange(img,(0/2,150,115),(35/2,255,255))
   #blue
   img = cv2.inRange(img,(216/2,133,67),(230/2,205,236))
   
-  
+
   #erode and dilate image
   img =  cv2.erode(img,np.ones((5,5)))
   img =  cv2.dilate(img,np.ones((10,10)))
@@ -40,7 +49,9 @@ def process(data):
 
 
   #contouring
-  contour = get_largest(img)
+  contour = [get_largest(img)]
+  
+  cv2.drawContours(img_og,contour,0,(0,255,0),3)
 
   #show images
   cv2.imshow("Image",img_og)
