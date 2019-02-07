6b8c1fe29ba09e354bd911bb7d26aff40797d982
Jonathan Heidegger <jth3141@gmail.com>
2019-02-06 19:25:29 -0500

    added the shell for ros integration with the BNO055

--- ros/src/i2c_com/scripts/i2c.py
+++ ros/src/i2c_com/scripts/i2c.py
@@ -1,6 +1,12 @@
 #! /usr/bin/python
 import rospy
+import time
+import smbus
+from BNO055 import BNO055
 from shared_msgs.msg import i2c_msg
+from shared_msgs.msg import imu_msg
+
+
 
 def message_received(msg):
   # This runs on a seperate thread from the pub
@@ -8,10 +14,18 @@ def message_received(msg):
 
 if __name__ == "__main__":
   rospy.init_node('i2c_node')
-  pub = rospy.Publisher('i2c_rx', i2c_msg,
-      queue_size= 100)
-  sub = rospy.Subscriber('i2c_tx', i2c_msg,
-      message_received)
+  imu_pub = rospy.Publisher('imu_data', imu_msg,
+	qeue_size = 100)
+  imu_pub.rate(50) 
+  imu = BNO055()
+  if not sensor:
+    raise RuntimeError('Failed to initialize BNO055!')
+  while not rospy.is_shutdown():
+    if imu.update():
+      out_message = imu_msg()
+      out_message.gyro = {imu.gyro_x, imu.gyro_y, imu.gyro_z}
+      out_message.accel = {imu.acceleration_x, imu.acceleration_y, imu.acceleration_z}
+      imu_pub.publish(msg)
+      r.sleep()
 
-  # TODO: I2C related activities
   rospy.spin()
