415c140cf2a0ca6e04ba1dadf268c88f65e6ed77
Jonathan Heidegger <jth3141@gmail.com>
2019-02-20 19:39:46 -0500

    added pressure sensor support to ROS

--- ros/src/i2c_com/scripts/i2c.py
+++ ros/src/i2c_com/scripts/i2c.py
@@ -4,11 +4,13 @@ import time
 import smbus
 from BNO055 import BNO055
 from TYS01 import TSYS01
+from ms5837 import MS5837
 from shared_msgs.msg import imu_msg
-from shared_msgs.msg import temp_message
+from shared_msgs.msg import temp_msg
+from shared_msgs.msg import depth_msg
 
 IMU_PUB_RATE = 50
-
+TEMP_RATIO_RATE = 4
 
 def message_received(msg):
   # This runs on a seperate thread from the pub
@@ -19,9 +21,12 @@ if __name__ == "__main__":
   imu_pub = rospy.Publisher('imu_data', imu_msg, qeue_size = 100)
   imu_pub.rate(IMU_PUB_RATE)
   temp_pub = rospy.Publisher('temp_data', temp_msg, qeue_size = 100)
+  depth_pub = rospy.Publisher('depth_data', depth_msg, qeue_size = 100)
   imu = BNO055()
   temp = TYS01()
+  depth = ms5837()
   temp.init()
+  temp_sleep = 0;
   while not rospy.is_shutdown():
     # using imu rate to poll the temp sensor as well beacuse IMU updates faster
     if imu.update():
@@ -30,9 +35,14 @@ if __name__ == "__main__":
       out_message.accel = {imu.acceleration_x, imu.acceleration_y, imu.acceleration_z}
       imu_pub.publish(out_message)
       # read the temp in C and send it to a ros
-      temp_message = temp_msg()
-      temp_message.temperature = {temp.temperature()}
-      temp_pub.publish(temp_message)
+      if(temp_sleep % TEMP_RATIO_RATE == 0) {
+          temp_message = temp_msg()
+          temp_message.temperature = {temp.temperature()}
+          temp_pub.publish(temp_message)
+      }
+      depth.update()
+      depth_message = depth_msg()
+      depth_msg.depth = {depth.depth()}
+      depth_pub.publish(depth_message)
+      temp_sleep++;
       r.sleep()
-
-  rospy.spin()
