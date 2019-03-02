36150316668a7e7c45d30649d26dde6eda7c0283
Lindsey May <may67@purdue.edu>
2019-03-02 15:15:04 -0500

    started implementing automatic control, no significant edits yet

--- ros/src/control/scripts/auto_control.py
+++ ros/src/control/scripts/auto_control.py
@@ -4,31 +4,34 @@ from shared_msgs.msg import can_msg, auto_control_msg, auto_command_msg
 from sensor_msgs.msg import Imu
 from std_msgs.msg import Float32
 
+imu_output = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
 def imu_received(msg):
-  # This runs on a seperate thread from the pub
-  pass
+  #set IMU output variable to message type based on ROS sensor_msgs standard
+  #might change to be specific to the output of our IMU -----------------------------------------------------
+  global imu_output = msg.orientation_covariance
 
 def depth_received(msg):
-  # This runs on a seperate thread from the pub
+  # do nothing for now; only focusing on roll, pitch, and yaw first
   pass
+  
 def command_received(msg):
-  # This runs on a seperate thread from the pub
   pass
 
 if __name__ == "__main__":
   rospy.init_node('auto_control')
 
+  #receive data 
   imu_sub = rospy.Subscriber('imu', Imu,
       imu_received)
-
   depth_sub = rospy.Subscriber('depth', Float32,
       depth_received)
-
   command_sub = rospy.Subscriber('/surface/auto_command',
     auto_command_msg, command_received)
 
+  #send data
   pub = rospy.Publisher('auto_control',
-    auto_control_msg, queue_size=10);
+    auto_control_msg, queue_size=10)
 
 
   rate = rospy.Rate(10) # 10hz
@@ -37,5 +40,3 @@ if __name__ == "__main__":
     sample_message = auto_control_msg()
     pub.publish(sample_message)
     rate.sleep()
-    
-
