855d730ee328faa333722e5c9c178a222a95e114
moldingtofu <jeremykchang@gmail.com>
2019-03-20 19:47:00 -0400

    converted back to socketio

--- ros/src/mux_demux/scripts/mux_demux.py
+++ ros/src/mux_demux/scripts/mux_demux.py
@@ -1,12 +1,13 @@
 #! /usr/bin/python
 import rospy
 import json
-import socket
+import socketio
+import engineio
+import eventlet
 import packet_mapper
 from shared_msgs.msg import can_msg, auto_command_msg, thrust_status_msg, thrust_command_msg, esc_single_msg
 from sensor_msgs.msg import Imu, Temperature
 from std_msgs.msg import Float32
-from StringIO import StringIO
 
 with open ('../../../../surface/frontend/src/packets.json') as json_data:
   data = json.load(json_data,)
@@ -17,57 +18,21 @@ flask_mapper = packet_mapper.packet_mapper(dearflask)
 client_mapper = packet_mapper.packet_mapper(dearclient)
 thrust_pub = None
 auto_pub = None
-serversocket = None
-clientsocket = None
 
-def serialize(data):
-  io = StringIO()
-  json.dump(data, io)
-  return io.getvalue()
+sio = socketio.Server()
+app = socketio.WSGIApp(sio, static_files={
+  '/': {'content_type': 'text/html', 'filename': 'index.html'}
+})
 
-def deserialize(data):
-  io = StringIO(data)
-  return json.load(io)
-
-def init_server():
-  global serversocket
-  serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  serversocket.bind((socket.gethostname(), 8000))
-  print('server started')
-  serversocket.listen(5)
-
-def listen():
-  global serversocket
-  global clientsocket
-
-  #listen for client
-  (clientsocket, address) = serversocket.accept()
-  print('client connected')
-
-def accept():
-  global serversocket
-  global clientsocket
+@sio.on('dearRos')
+def accept(sid, data):
   global dearflask
   global dearclient
 
-  length = clientsocket.recv(10)
-
-  try:
-    length = int(length)
-  except:
-    raise Exception()
-
-  dearflask = deserialize(clientsocket.recv(length))
+  dearflask = data
 
   #pass back dearclient
-  encode = serialize(dearclient)
-  head = str(len(encode))
-
-  for x in range(10 - len(head)):
-    head = '0' + head
-
-  encode = head + encode
-  clientsocket.send(encode)
+  sio.emit('dearclient-response', dearclient)
 
   #update thrust and auto
   #flask_mapper.pam(thrust_command_msg(), dearflask)
@@ -78,13 +43,13 @@ def accept():
   return dearflask
 
 def name_received(msg):
+  global dearclient
+
   names = client_mapper.get_msg_vars(msg)
   for name in names:
     client_mapper.map(name, getattr(msg, name), dearclient)
 
 if __name__ == "__main__":
-  global clientsocket
-
   rospy.init_node('mux_demux')
   ns = rospy.get_namespace() # This should return /surface
   
@@ -114,13 +79,8 @@ if __name__ == "__main__":
   auto_pub = rospy.Publisher(ns +'auto_command',
     auto_command_msg, queue_size=10);
 
-  init_server()
-  rate = rospy.Rate(10)
-  while not rospy.is_shutdown():
-    try:
-      data = accept()
-      print data
-    except:
-      print('no client connected')
-      listen()
-    rate.sleep()
+  eventlet.wsgi.server(eventlet.listen(('', 5001)), app)
+
+  #rate = rospy.Rate(10)
+  #while not rospy.is_shutdown():
+  #  rate.sleep()
