551be4708f7475068ffc81a462878645224bc977
Lindsey May <may67@purdue.edu>
2019-03-27 19:59:12 -0400

    added publishing data to every time auto_control receives data

--- ros/src/control/scripts/auto_control.py
+++ ros/src/control/scripts/auto_control.py
@@ -4,19 +4,28 @@ from shared_msgs.msg import can_msg, auto_control_msg, auto_command_msg
 from sensor_msgs.msg import Imu
 from std_msgs.msg import Float32
 
+setpoints = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 imu_output = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+thrust_data = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
 def imu_received(msg):
   #set IMU output variable to message type based on ROS sensor_msgs standard
   #might change to be specific to the output of our IMU -----------------------------------------------------
-  global imu_output = msg.orientation_covariance
+  global imu_output
+  imu_output = msg.orientation_covariance
+
+  #TODO: send data and setpoints to pid object (make pid object global?)
 
 def depth_received(msg):
   # do nothing for now; only focusing on roll, pitch, and yaw first
   pass
   
 def command_received(msg):
-  pass
+  sample_message = auto_control_msg()
+  sample_message.thrust_vec = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+  sample_message.dims_locked = [False, False, False, False, False, False]
+  pub.publish(sample_message)
+    
 
 if __name__ == "__main__":
   rospy.init_node('auto_control')
@@ -33,10 +42,14 @@ if __name__ == "__main__":
   pub = rospy.Publisher('auto_control',
     auto_control_msg, queue_size=10)
 
+  #TODO: initialize the pid object
 
   rate = rospy.Rate(10) # 10hz
-  # TODO: I2C related activities
+
   while not rospy.is_shutdown():
     sample_message = auto_control_msg()
+    sample_message.thrust_vec = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    sample_message.dims_locked = [False, False, False, False, False, False]
+    #TODO: set sample_message to thrust_data
     pub.publish(sample_message)
     rate.sleep()
