3541571b28204a189fb6bdba72a5fcf2e43218e5
Nicolas Fransen <me@nicolasfransen.com>
2019-03-30 14:55:28 -0400

    TaskManifest created

---
+++ surface/frontend/src/components/TaskList/Task.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Task extends Component {
+  render() {
+    return (
+      <div>
+        Task Works!
+      </div>
+    )
+  }
+}
---
+++ surface/frontend/src/components/TaskList/TaskList.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Task from "./Task.jsx";
+
+export default class TaskList extends Component {
+  render() {
+    return (
+      <div>
+        TaskList Works!
+      </div>
+    )
+  }
+}
