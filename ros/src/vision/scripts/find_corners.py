e09125911318c96828d113d8dd32a9e8e251be44
Zach Ghera <zpghera00@gmail.com>
2019-05-14 16:44:15 -0400

    working line identification portion of find corners. Now need to grab point of corner and put in logic for when corners exist

--- ros/src/vision/scripts/find_corners.py
+++ ros/src/vision/scripts/find_corners.py
@@ -16,20 +16,24 @@ def find_corners(img):
   edges = cv2.Canny(gray, 50, 200, 3)
   lines = cv2.HoughLines(edges, 1, np.pi/180, 220)
 
-  for line in lines:
-    rho = line[0][0]
-    theta = line[0][1]
+  try:
+    for line in lines:
+      rho = line[0][0]
+      theta = line[0][1]
 
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a * rho
-    y0 = b * rho
-    x1 = int(x0 + 1000*(-b))
-    y1 = int(y0 + 1000*(a))
-    x2 = int(x0 - 1000*(-b))
-    y2 = int(y0 - 1000*(a))
+      a = np.cos(theta)
+      b = np.sin(theta)
+      x0 = a * rho
+      y0 = b * rho
+      x1 = int(x0 + 1000*(-b))
+      y1 = int(y0 + 1000*(a))
+      x2 = int(x0 - 1000*(-b))
+      y2 = int(y0 - 1000*(a))
 
-    cv2.line(img, (x1,y1), (x2,y2), (0,0,255), 2)
+      cv2.line(img, (x1,y1), (x2,y2), (0,255,0), 2)
+  except TypeError:
+    print("No lines found above set threshold")
+    return None
 
   return None
 
